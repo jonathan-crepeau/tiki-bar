68e1b14eb6a47c4c76970c7516b1f874e921423b
johnofhyrule <johnroman7@mac.com>
2020-01-29 12:59:44 -0800

    begins code on event listener

--- public/scripts/signup.js
+++ public/scripts/signup.js
@@ -1,1 +1,12 @@
 console.log('This is signup...');
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    fetch('/views/dash', {
+        method: 'POST',
+    })
+        .then((dataStream) => dataStream.json())
+        .then((data) => window.location = '/views/success')
+        .catch((error) => console.log(error))
+});
